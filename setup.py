f33bcf23ee71f1577c1aac4873398c2ecaf32be0
cuileann <cuileann@outlook.ie>
2018-02-23 09:40:54 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,60 @@
-# from flask documentation
-# entry points from Aongus' slides
-from setuptools import setup,find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
 setup(
-    version=1,
-    name='systeminfo_flask',
-    packages=['app'],
-    include_package_data=True,
-    install_requires=['flask'],
+    author="Colin Sheehan",
+    author_email='colinjohn@outlook.ie',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="application outputs system details from machine it runs on",
+
     entry_points={
     'console_scripts':
     [
         'platform_info=app.run:main'
-    ]}
+    ]},
 
+    install_requires=['flask'],
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='systeminfo_flask',
+    name='systeminfo_flask',
+
+
+    packages=['app'],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+
+    url='https://github.com/colinjsheehan/systeminfo_flask.git',
+
+    version='1.1',
+    zip_safe=False,
 )
